d3742c60375061b8be28712d47a5016ad9ef600c
Daniel Kim <chipperday@gmail.com>
2022-08-11 05:34:04 +0000

    aaa

--- pages/_app.js
+++ pages/_app.js
@@ -1,42 +1,92 @@
-import Head from 'next/head';
-import { useEffect, useState } from 'react';
-import SiteContext from '../lib/site-context';
-import '../styles/globals.css';
+import Head from "next/head";
+import { useEffect, useState } from "react";
+import SiteContext from "../lib/site-context";
+import "../styles/globals.css";
 import { initializeAuth } from "firebase/auth";
-import { firebaseApp } from '../lib/firebase';
-import axios from 'axios';
+import { firebaseApp } from "../lib/firebase";
+import axios from "axios";
+import { onLog } from "firebase/app";
 
-export default function MyApp({ Component, pageProps: { session, ...pageProps } }) {
-    let [place, setPlace] = useState(null);
-    let [pageTitle, setPageTitle] = useState("Box Spots");
+export default function MyApp({
+  Component,
+  pageProps: { session, ...pageProps },
+}) {
+  let [place, setPlace] = useState(null);
+  let [pageTitle, setPageTitle] = useState("Box Spots");
+  let [loading, setLoading] = useState(true);
 
-    useEffect(() => {
-        let fauth = initializeAuth(firebaseApp);
-        let unsub = fauth.onAuthStateChanged(fb => fb ? onLogin(fb) : onLogout());
+  useEffect(() => {
+    let data = sessionStorage.getItem("isLoggedIn");
+    let fauth = initializeAuth(firebaseApp);
 
-        return () => unsub();
-    }, []);
-
-    const onLogin = async (fb) => {
-        let token = await fb.getIdToken(false);
-        let p = (await axios.post(`/api/auth/place`, undefined, { headers: { authorization: `bearer ${token}` } })).data;
-
-        if (p.first_name?.length) setPageTitle("Welcome " + p.first_name);
-
-        setPlace(p);
+    if (data) {
+      setTimeout(() => {
+        setLoading(false);
+      }, 2000);
+    } else {
+      setLoading(false);
     }
 
-    const onLogout = () => {
-        setPlace(null);
-    }
+    let unsub = fauth.onAuthStateChanged((fb) => {
+      if (fb) {
+        setLoading(false);
+        onLogin(fb);
+      } else {
+        // onLogout();
+      }
+    });
 
-    return (
-        <SiteContext.Provider value={{ place, setPageTitle }}>
-            <Head>
-                <title>{pageTitle}</title>
-            </Head>
+    return () => unsub();
+  }, []);
 
-            <Component {...pageProps} />
-        </SiteContext.Provider>
-    );
+  const onLogin = async (fb) => {
+    let token = await fb.getIdToken(false);
+    let p = (
+      await axios.post(`/api/auth/place`, undefined, {
+        headers: { authorization: `bearer ${token}` },
+      })
+    ).data;
+    if (p.first_name?.length) setPageTitle("Welcome " + p.first_name);
+    setPlace(p);
+
+    // sessionStorage.setItem("p", JSON.stringify(p));
+    localStorage.setItem("isLoggedIn", "true");
+  };
+
+  const onLogout = () => {
+    let fauth = initializeAuth(firebaseApp);
+    signOut(fauth);
+
+    setPlace(null);
+    localStorage.removeItem("isLoggedIn");
+  };
+
+  return (
+    <SiteContext.Provider value={{ place, setPageTitle, onLogout }}>
+      <Head>
+        <title>{pageTitle}</title>
+      </Head>
+
+      {loading && <FullScreenLoading />}
+      <Component {...pageProps} />
+    </SiteContext.Provider>
+  );
+}
+
+
+function FullScreenLoading()  {
+    return <div style={{
+        position: "fixed",
+        top: 0,
+        left: 0,
+        right: 0,
+        bottom: 0,
+        background: "rgba(0, 0, 0, 0.5)",
+        display: "flex",
+        alignItems: "center",
+        justifyContent: "center",
+        fontSize: 40
+    }}>
+        loading...
+    </div>
 }
--- pages/index.jsx
+++ pages/index.jsx
@@ -1,67 +1,93 @@
-import Image from 'next/image';
-import Link from 'next/link';
-import { useCallback, useEffect, useState, useRef } from 'react';
-import styles from '../styles/Home.module.scss';
+import { signOut } from "firebase/auth";
+import Image from "next/image";
+import Link from "next/link";
+import { useCallback, useEffect, useState, useRef, useContext } from "react";
+import SiteContext from "../lib/site-context";
+import styles from "../styles/Home.module.scss";
 
 export default function Home() {
-    let page = useRef();
-    let [y, setY] = useState(0);
+  let page = useRef();
+  let [y, setY] = useState(0);
+  let { place, onLogout } = useContext(SiteContext);
 
-    const onScroll = useCallback(e => {
-        let y = e.currentTarget.scrollTop;
-        setY(y);
-    }, []);
+  const onScroll = useCallback((e) => {
+    let y = e.currentTarget.scrollTop;
+    setY(y);
+  }, []);
 
-    useEffect(() => {
-        let pc = page.current;
+  useEffect(() => {
+    let pc = page.current;
 
-        setY(pc.scrollTop);
-        pc.addEventListener("scroll", onScroll);
+    setY(pc.scrollTop);
+    pc.addEventListener("scroll", onScroll);
 
-        return () => pc.removeEventListener("scroll", onScroll);
-    }, [onScroll]);
+    return () => pc.removeEventListener("scroll", onScroll);
+  }, [onScroll]);
 
-    return (
-        <div ref={page} className={styles.page}>
-            <nav className={`${styles.nav} ${y > 0 ? styles.visible : undefined}`}>
-                <div>
-                    <Image src="/logo1.png" alt="" width={60} height={60} />
-                    <h1>Box Spots</h1>
-                </div>
-
-                <div>
-                    <Link href="/download"><a style={{ marginRight: 25 }}>Download App</a></Link>
-                    <Link href="/app"><a>Console</a></Link>
-                </div>
-            </nav>
-
-            <div className={styles.main}>
-                <h1>Explore with Pattern</h1>
-                <p>For museums, amusement parks, conferences, markets, special events, and more!</p>
-                <div>
-                    <Link href="/download"><a style={{ marginRight: 25 }}>Tourists</a></Link>
-                    <Link href="/app"><a>Businesses</a></Link>
-                </div>
-            </div>
-
-            <div className={styles.about}>
-                <h1>About Box Spots</h1>
-                <h3>
-                    Age of paper pamphletes has come to an end!
-                    Box Spots is an interactive map conveying useful features for businesses and tourists.
-                    Just by scanning the QR code placed near each display, users can view specific descriptions and receive recommendations on where to go next.
-                    For more information, please contact us below.
-                </h3>
-            </div>
-
-            <div className={styles.contact}>
-                <h1>Contact Us</h1>
-                <div>
-                    <h3>Email: <a href="mailto:contact@theboxspots.com">contact@theboxspots.com</a></h3>
-                </div>
-            </div>
-
-            <footer className={styles.footer}>Copyright 2022. Box Spots. All rights reserved.</footer>
+  return (
+    <div ref={page} className={styles.page}>
+      <nav className={`${styles.nav} ${y > 0 ? styles.visible : undefined}`}>
+        <div>
+          <Image src="/logo1.png" alt="" width={60} height={60} />
+          <h1>Box Spots</h1>
         </div>
-    );
+
+        <div>
+          <Link href="/download">
+            <a style={{ marginRight: 25 }}>Download App</a>
+          </Link>
+          <Link href="/app">
+            <a>Console</a>
+          </Link>
+        </div>
+
+        {place ? (
+          <button onClick={onLogout}>Log out</button>
+        ) : (
+          <button>Log in</button>
+        )}
+      </nav>
+
+      <div className={styles.main}>
+        <h1>Explore with Pattern</h1>
+        <p>
+          For museums, amusement parks, conferences, markets, special events,
+          and more!
+        </p>
+        <div>
+          <Link href="/download">
+            <a style={{ marginRight: 25 }}>Tourists</a>
+          </Link>
+          <Link href="/app">
+            <a>Businesses</a>
+          </Link>
+        </div>
+      </div>
+
+      <div className={styles.about}>
+        <h1>About Box Spots</h1>
+        <h3>
+          Age of paper pamphletes has come to an end! Box Spots is an
+          interactive map conveying useful features for businesses and tourists.
+          Just by scanning the QR code placed near each display, users can view
+          specific descriptions and receive recommendations on where to go next.
+          For more information, please contact us below.
+        </h3>
+      </div>
+
+      <div className={styles.contact}>
+        <h1>Contact Us</h1>
+        <div>
+          <h3>
+            Email:{" "}
+            <a href="mailto:contact@theboxspots.com">contact@theboxspots.com</a>
+          </h3>
+        </div>
+      </div>
+
+      <footer className={styles.footer}>
+        Copyright 2022. Box Spots. All rights reserved.
+      </footer>
+    </div>
+  );
 }
